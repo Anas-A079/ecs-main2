67fd71b614a318b76f82adf82e917e8a9c94d6eb
Anas-A079 <079anaswork@gmail.com>
2026-04-30 23:39:57 +0100

    add terraform variables for domain, hosted zone, and image url

--- app/Infra/variables.tf
+++ app/Infra/variables.tf
@@ -5,3 +5,18 @@ variable "app_name" {
 variable "container_port" {
   default = 80
 }
+
+variable "domain_name" {
+  description = "Domain name for the application"
+  type        = string
+}
+
+variable "hosted_zone_name" {
+  description = "Route53 hosted zone name"
+  type        = string
+}
+
+variable "image_url" {
+  description = "ECR image URL for ECS task"
+  type        = string
+}
