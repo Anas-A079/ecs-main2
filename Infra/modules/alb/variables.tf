da1bc52f5cf5a95c4021665561a74370fd3f9410
Anas-A079 <079anaswork@gmail.com>
2026-05-21 01:32:06 +0100

    Fix terraform variables.tf issues

--- Infra/modules/alb/variables.tf
+++ Infra/modules/alb/variables.tf
@@ -1,15 +1,19 @@
-output "alb_dns_name" {
-  value = aws_lb.this.dns_name
+variable "app_name" {
+  type = string
 }
 
-output "alb_zone_id" {
-  value = aws_lb.this.zone_id
+variable "vpc_id" {
+  type = string
 }
 
-output "target_group_arn" {
-  value = aws_lb_target_group.this.arn
+variable "subnet_ids" {
+  type = list(string)
 }
 
-output "alb_security_group_id" {
-  value = aws_security_group.alb.id
+variable "container_port" {
+  type = number
+}
+
+variable "certificate_arn" {
+  type = string
 }
